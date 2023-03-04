4b785b7422d5b6aecbcc5d0fd6d6c2e95e537ecb
Bartek <bartlomiejtolak7@gmail.com>
2023-03-04 14:09:29 +0100

    [Change] -new lambdas

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -8,13 +8,27 @@ public class LambdaExercises {
         boy.silnoreki();
         //boy.sing(); <---- to nie zadziałą
         // zadziała jedynie przez rzutowanie
-        ((ChlopakiNiePlacza)boy).sing();
+        ((ChlopakiNiePlacza) boy).sing();
 
-
-
-        ChlopakiNiePlacza boy2=new ChlopakiNiePlacza();
+        ChlopakiNiePlacza boy2 = new ChlopakiNiePlacza();
         boy2.sing();
 
+        // klasa anonimowa
+        BoysDontCry anonymousClassReference = new BoysDontCry() {
+            @Override
+            public void silnoreki() {
+                System.out.println("Anonimoy silnoreki");
+            }
+        };
+        anonymousClassReference.silnoreki();
+
+        BoysDontCry firstLambda = () -> System.out.println("lambda silnoreki");
+        firstLambda.silnoreki();
+
+        BoysDontCry secondLambda =() ->{
+            System.out.println("one");
+            System.out.println("second");
+        };
 
 
     }
