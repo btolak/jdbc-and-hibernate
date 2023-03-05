ebf64bea5bdd1923543d8c03bd752ee79f017c6b
Bartek <bartlomiejtolak7@gmail.com>
2023-03-05 15:57:23 +0100

    [change] - trying to resolve problem with No driver found

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -2,19 +2,29 @@ package pl.sda.orange2.jdbc;
 
 import pl.sda.orange2.jdbc.config.H2Config;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TestConnection {
     public static void main(String[] args) {
+        String query = """
+                select id,name
+                from test
+                """;
+
         try {
 
-         var h2Connection =   DriverManager.getConnection(H2Config.DB_URL,
+            var h2Connection =DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
-            System.out.println("got connection: " +(h2Connection !=null));
+            System.out.println("got connection: " + (h2Connection != null));
+            Statement queryStatement = h2Connection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(query);
+            System.out.println("id: " + queryResult.getInt(1));
+            System.out.println("name: " + queryResult.getString(2));
         } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
+        exc.printStackTrace();
         }
     }
 }
+
