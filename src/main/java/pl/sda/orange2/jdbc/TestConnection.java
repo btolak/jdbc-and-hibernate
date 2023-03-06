37175f1c9f85a4840c7f7a6d3db0b06d46c1a088
Bartek <bartlomiejtolak7@gmail.com>
2023-03-06 10:58:13 +0100

    [change] - comments and formatting

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -12,19 +12,26 @@ public class TestConnection {
                 """;
 
         try {
-
-            var h2Connection =DriverManager.getConnection(H2Config.DB_URL,
+            // we need jdbc driver (h2 dependency at pom)
+            // connection gave us access to existing db
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
             System.out.println("got connection: " + (h2Connection != null));
+
+            // Statement is used to send queries to db with existing connection
             Statement queryStatement = h2Connection.createStatement();
+
+            // ResultSet contains query result data as simple table
+            // we need to iterate over result to got data
             ResultSet queryResult = queryStatement.executeQuery(query);
-            while(queryResult.next()) {
+            while (queryResult.next()) {
                 System.out.println("id: " + queryResult.getInt(1));
                 System.out.println("name: " + queryResult.getString(2));
-            }} catch (SQLException exc) {
+            }
+        } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
-        exc.printStackTrace();
+            exc.printStackTrace();
         }
     }
 }
