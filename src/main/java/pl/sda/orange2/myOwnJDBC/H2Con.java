14ae66a09cac3ea41b55ca9467e08e0851e3bfd5
Bartek <bartlomiejtolak7@gmail.com>
2023-03-08 10:51:15 +0100

    [change] - change directory of db

--- src/main/java/pl/sda/orange2/myOwnJDBC/H2Con.java
+++ src/main/java/pl/sda/orange2/myOwnJDBC/H2Con.java
@@ -1,7 +1,7 @@
 package pl.sda.orange2.myOwnJDBC;
 
 public class H2Con {
-    public final static String DB_URL = "jdbc:h2:C:\\Users\\sobot\\OneDrive\\Desktop\\myOwnJDBC";
+    public final static String DB_URL = "jdbc:h2:C:\\Users\\sobot\\OneDrive\\Desktop\\myOwnJDBc\\myOwnJDBC";
     public final static String USER = "sa";
     public final static String PASSWORD = "sa";
 }
