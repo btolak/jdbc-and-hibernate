bfc426f07d8ee275dc02970f5908f2c8199ded3a
Bartek <bartlomiejtolak7@gmail.com>
2023-03-09 18:04:46 +0100

    [change] - generic DataAccess dao created

--- src/main/java/pl/sda/orange2/dao/CarDao.java
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -4,17 +4,21 @@ import pl.sda.orange2.entity.Car;
 
 import java.util.List;
 
-public class CarDao {
-    public void save(Car car){
-
+// CRUD - Create Read Update Delete
+public class CarDao implements DataAccess<Car, Long> {
+    @Override
+    public void save(Car car) {
     }
-    public List<Car> findAll(){
+    @Override
+    public List<Car> findAll() {
         return List.of();
     }
-    public Car findById(Long id){
+    @Override
+    public Car findById(Long id) {
         return null;
     }
-    public void deleteById(Long id){
+    @Override
+    public void deleteById(Long id) {
 
     }
 }
