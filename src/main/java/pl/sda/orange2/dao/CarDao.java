af413b89671d87a5de93589a488b98de1ff13c4a
Bartek <bartlomiejtolak7@gmail.com>
2023-03-09 19:35:38 +0100

    [change] - findAll and finById dao methods implemented

--- src/main/java/pl/sda/orange2/dao/CarDao.java
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -2,21 +2,86 @@ package pl.sda.orange2.dao;
 
 import pl.sda.orange2.entity.Car;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 // CRUD - Create Read Update Delete
 public class CarDao implements DataAccess<Car, Long> {
+    private final Connection dbConnection;
+
+    //CREATE TABLE CARS(
+    //ID BIGINT AUTO_INCREMENT PRIMARY KEY ,
+    //COLOUR VARCHAR(255),
+    //BRAND VARCHAR(255),
+    //MODEL VARCHAR(255)
+    //);
+    public CarDao(Connection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
     @Override
     public void save(Car car) {
     }
+
     @Override
     public List<Car> findAll() {
-        return List.of();
+        // var is te same as: List<Car> cars = new ArrayList<>();
+        var cars = new ArrayList<Car>();
+
+        //TODO: validate query
+        String allCarsQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                 FROM CARS""";
+        try {
+            Statement queryStatement = dbConnection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(allCarsQuery);
+            while (queryResult.next()) {
+
+                Long id = queryResult.getLong(1);
+                String colour = queryResult.getString(2);
+                String brand = queryResult.getString(3);
+                String model = queryResult.getString(4);
+
+                Car carFromDb = new Car(id, colour, brand,model);
+                cars.add(carFromDb);
+            }
+        } catch (SQLException e) {
+
+            System.out.println("Unexpected sql exception occurred");
+            e.printStackTrace();
+        }
+
+        return cars;
     }
+
     @Override
     public Car findById(Long id) {
-        return null;
+        Car result = null;
+        String carByIdQuery = """
+                SELECT ID, COLOUR, BRAND, MODEL
+                 FROM CARS
+                 WHERE ID = ? """;
+        try {
+            PreparedStatement queryStatement = dbConnection.prepareStatement(carByIdQuery);
+            queryStatement.setLong(1,id);
+            ResultSet queryResult = queryStatement.executeQuery();
+           if(queryResult.next()){
+               result=new Car(
+                       queryResult.getLong("ID"),
+                       queryResult.getString("COLOUR"),
+                       queryResult.getString("BRAND"),
+                       queryResult.getString("MODEL")
+               );
+           }
+        } catch (SQLException e) {
+
+            System.out.println("Unexpected sql exception occurred");
+            e.printStackTrace();
+        }
+        return result;
     }
+
     @Override
     public void deleteById(Long id) {
 
